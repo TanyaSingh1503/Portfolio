ddcca2d7eadd854281fcdfebd0ad9de1eddb20b9
Tanya Singh <tststanya1503@gmail.com>
2025-11-29 22:22:50 +0530

    resume updated

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
         </li>
         <li className="flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300">
           <a
-            href="/girishupdate30.pdf" // Change this to your actual resume link
+            href="/Tanya Singh Data Analyst.pdf" // Change this to your actual resume link
             target="_blank"
             rel="noopener noreferrer"
             className="flex justify-between items-center w-full text-white"
